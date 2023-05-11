a44b8adf1834bbff2f9fe553083fa252861fd66d
MarioSergDC <mariosergdc.webdev@gmail.com>
2023-05-11 11:56:38 -0400

    Folder structure for using Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Provider } from 'react-redux';
+import store from './store';
 import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <h1>Training App</h1>
-    </div>
+    <Provider store={store}>
+      <div className="app">
+        <h1>Training App</h1>
+      </div>
+    </Provider>
   );
 }
 
