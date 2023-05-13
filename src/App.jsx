de3b895a1c66d8d521b13a655eb719d05908aa37
MarioSergDC <mariosergdc.webdev@gmail.com>
2023-05-12 22:16:33 -0400

    adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Provider } from 'react-redux';
 import store from './store';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="app">
-        <h1>Training App</h1>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>Training App Home</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/contact" element={<h1>Contact</h1>} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
