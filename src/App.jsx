0056e26a8bd714abf7806f11d76549f0e485da37
MarioSergDC <mariosergdc.webdev@gmail.com>
2023-05-12 22:31:07 -0400

    fixing imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import { Provider } from 'react-redux';
-import store from './store';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TaskForm from './components/TaskForm';
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<h1>Training App Home</h1>} />
-          <Route path="/about" element={<h1>About</h1>} />
-          <Route path="/new-task" element={<TaskForm />} />
-          <Route path="/contact" element={<h1>Contact</h1>} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Training App Home</h1>} />
+        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/new-task" element={<TaskForm />} />
+        <Route path="/contact" element={<h1>Contact</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
